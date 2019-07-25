2f48edecb2bf9532c66db950becec7c1383069ed
turquoise-turtle <16552560+turquoise-turtle@users.noreply.github.com>
2019-07-25 17:45:56 +1000

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,43 @@
 /*partly from https://webapps.stackexchange.com/questions/65758/how-do-i-skip-404s-in-the-wayback-machine*/
 /*the 'javascript:' thing turns it into a bookmarklet, remove for actual use */
-javascript:var f = function(jsn){if (jsn.archived_snapshots.closest) {window.location.href=jsn.archived_snapshots.closest.url.replace('http://web', 'https://web');} else { window.location.href='http://webcache.googleusercontent.com/search?q=cache:' + window.location.href; /*alert('unfortunately there\'s no cached copy')*/}};var d = document,z=d.createElement('script'),b=d.body,l=d.location;z.setAttribute('src','https://archive.org/wayback/available?url='+encodeURIComponent(l.href)+'&callback=f');z.setAttribute('type', "application/javascript");b.appendChild(z);void(0);
+
+//v1
+javascript:var f = function(jsn){if (jsn.archived_snapshots.closest) {window.location.href=jsn.archived_snapshots.closest.url.replace('http://web', 'https://web');} else { window.location.href='http://webcache.googleusercontent.com/search?q=cache:' + window.location.href; /*alert('unfortunately there\'s no cached copy')*/}};var d = document,z=d.createElement('script'),b=d.body,l=d.location;z.setAttribute('src','https://archive.org/wayback/available?url='+encodeURIComponent(l.href)+'&callback=f');z.setAttribute('type', "application/javascript");b.appendChild(z);void(0);/*partly from https://webapps.stackexchange.com/questions/65758/how-do-i-skip-404s-in-the-wayback-machine*/
+
+
+javascript:
+var f = function(jsn){
+	if (jsn.archived_snapshots.closest) {
+		window.location.href=jsn.archived_snapshots.closest.url.replace('http://web', 'https://web');
+	} else {
+		window.location.href='http://webcache.googleusercontent.com/search?q=cache:' + window.location.href; 
+		/*alert('unfortunately there\'s no cached copy')*/
+	}
+};
+var d = document,z=d.createElement('script'),b=d.body,l=d.location;
+z.setAttribute('src','https://archive.org/wayback/available?url='+encodeURIComponent(l.href)+'&callback=f');
+z.setAttribute('type', "application/javascript");
+b.appendChild(z);
+void(0);
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		// document.getElementById("demo").innerHTML = xhttp.responseText;
+		var jsn = xhttp.response;
+		if (jsn.archived_snapshots.closest) {
+			window.location.href=jsn.archived_snapshots.closest.url.replace('http://web', 'https://web');
+		}
+	} else {
+		window.location.href='http://webcache.googleusercontent.com/search?q=cache:' + window.location.href; 
+	}
+};
+xhttp.open("GET", 'https://archive.org/wayback/available?url='+encodeURIComponent(window.location.href), true);
+xhttp.send();
+
+//v2
+javascript:var xhttp=new XMLHttpRequest;xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status){var t=xhttp.response;t.archived_snapshots.closest&&(window.location.href=t.archived_snapshots.closest.url.replace("http://web","https://web"))}else window.location.href="http://webcache.googleusercontent.com/search?q=cache:"+window.location.href},xhttp.open("GET","https://archive.org/wayback/available?url="+encodeURIComponent(window.location.href),!0),xhttp.send();
+
+//v1b
+javascript:var f = function(jsn){if (jsn.archived_snapshots.closest) {window.location.href=jsn.archived_snapshots.closest.url.replace('http://web', 'https://web');} else { window.location.href='http://webcache.googleusercontent.com/search?q=cache:' + window.location.href; /*alert('unfortunately there\'s no cached copy')*/}};var d = document,z=d.createElement('script'),b=d.body,l=d.location;z.setAttribute('src','https://archive.org/wayback/available?url='+encodeURIComponent(l.href)+'&callback=f');b.appendChild(z);void(0);/*partly from https://webapps.stackexchange.com/questions/65758/how-do-i-skip-404s-in-the-wayback-machine*/
