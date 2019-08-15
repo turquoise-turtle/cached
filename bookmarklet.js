138f2f23f6fe57e84020dea6e58d8d978204a70d
turquoise-turtle <16552560+turquoise-turtle@users.noreply.github.com>
2019-08-15 13:36:58 +1000

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,18 @@
+//v2
+javascript:var xhttp=new XMLHttpRequest;xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status){var t=xhttp.response;t.archived_snapshots.closest&&(window.location.href=t.archived_snapshots.closest.url.replace("http://web","https://web"))}else window.location.href="http://webcache.googleusercontent.com/search?q=cache:"+window.location.href},xhttp.open("GET","https://archive.org/wayback/available?url="+encodeURIComponent(window.location.href),!0),xhttp.send();
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*partly from https://webapps.stackexchange.com/questions/65758/how-do-i-skip-404s-in-the-wayback-machine*/
 /*the 'javascript:' thing turns it into a bookmarklet, remove for actual use */
 
@@ -37,9 +52,6 @@ xhttp.onreadystatechange = function() {
 };
 xhttp.open("GET", 'https://new-tab-cors-server.herokuapp.com/https://archive.org/wayback/available?url=' + encodeURIComponent(window.location.href), true);
 xhttp.send();
-
-//v2
-javascript:var xhttp=new XMLHttpRequest;xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status){var t=xhttp.response;t.archived_snapshots.closest&&(window.location.href=t.archived_snapshots.closest.url.replace("http://web","https://web"))}else window.location.href="http://webcache.googleusercontent.com/search?q=cache:"+window.location.href},xhttp.open("GET","https://archive.org/wayback/available?url="+encodeURIComponent(window.location.href),!0),xhttp.send();
 
 //v1b
 javascript:var f = function(jsn){if (jsn.archived_snapshots.closest) {window.location.href=jsn.archived_snapshots.closest.url.replace('http://web', 'https://web');} else { window.location.href='http://webcache.googleusercontent.com/search?q=cache:' + window.location.href; /*alert('unfortunately there\'s no cached copy')*/}};var d = document,z=d.createElement('script'),b=d.body,l=d.location;z.setAttribute('src','https://archive.org/wayback/available?url='+encodeURIComponent(l.href)+'&callback=f');b.appendChild(z);void(0);/*partly from https://webapps.stackexchange.com/questions/65758/how-do-i-skip-404s-in-the-wayback-machine*/
