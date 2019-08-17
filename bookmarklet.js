e242530a71dd778b9180642d7a0a202b5d161ff7
turquoise-turtle <16552560+turquoise-turtle@users.noreply.github.com>
2019-08-17 18:39:28 +1000

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 //v2
-javascript:var xhttp=new XMLHttpRequest;xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status){var t=xhttp.response;t.archived_snapshots.closest&&(window.location.href=t.archived_snapshots.closest.url.replace("http://web","https://web"))}else window.location.href="http://webcache.googleusercontent.com/search?q=cache:"+window.location.href},xhttp.open("GET","https://archive.org/wayback/available?url="+encodeURIComponent(window.location.href),!0),xhttp.send();
+javascript:var xhttp=new XMLHttpRequest;xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status){var t=JSON.parse(xhttp.responseText);t.archived_snapshots.closest&&(window.location.href=t.archived_snapshots.closest.url.replace("http://web","https://web"))}else window.location.href="http://webcache.googleusercontent.com/search?q=cache:"+window.location.href},xhttp.open("GET","https://new-tab-cors-server.herokuapp.com/wm/"+window.location.href,!0),xhttp.send();
 
 
 
@@ -55,3 +55,4 @@ xhttp.send();
 
 //v1b
 javascript:var f = function(jsn){if (jsn.archived_snapshots.closest) {window.location.href=jsn.archived_snapshots.closest.url.replace('http://web', 'https://web');} else { window.location.href='http://webcache.googleusercontent.com/search?q=cache:' + window.location.href; /*alert('unfortunately there\'s no cached copy')*/}};var d = document,z=d.createElement('script'),b=d.body,l=d.location;z.setAttribute('src','https://archive.org/wayback/available?url='+encodeURIComponent(l.href)+'&callback=f');b.appendChild(z);void(0);/*partly from https://webapps.stackexchange.com/questions/65758/how-do-i-skip-404s-in-the-wayback-machine*/
+javascript:(function(){ document.body.innerHTML = '';var s = "\n\tbody {\n\t\tmargin: 0;\n\t}\n\tiframe {\n\t\twidth: 100vw;\n\t\theight: 100vh;\n\t\tborder: 0;\n\t\tmargin: 0;\n\t\tpadding: 0;\n\t\tz-index:99999;\n\t}\n";var y = document.createElement('style');y.innerText = s;document.body.appendChild(y);var x = document.createElement('iframe');x.src = window.atob('aHR0cHM6Ly92aWEuaHlwb3RoZXMuaXMv') + location.href;document.body.appendChild(x);document.title = location.href; })();
